ecad59861f63279abd2eadd964565d6aed46387a
pbragamiranda <pbragamiranda@gmail.com>
2022-08-15 18:49:34 +0200

    airbnb clone

---
+++ test/system/flats_test.rb
@@ -0,0 +1,10 @@
+require "application_system_test_case"
+
+class FlatsTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit flats_url
+    # take_and_open_screenshot
+
+    assert_selector "h1", text: "Flats"
+  end
+end
